fc0cde587483c006bf28482fd67685027e041122
Sadat-Shahriyar <shahriyarsadat@gmail.com>
2020-07-05 13:44:01 +0600

    config.js updated

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     'mongoUrl': 'mongodb://127.0.0.1/confusion',
-    'secretKey': 'tt142857',
+    'secretKey': 'secretk key you want to use',
     'facebook': {
-        clientId: '3373425382690523',
-        clientSecret: '6fafa9899e78e48f11efecede68004bd'
+        clientId: 'client id from developer.facebook.com',
+        clientSecret: 'client secret from developer.facebook.com'
     }
 }
 
